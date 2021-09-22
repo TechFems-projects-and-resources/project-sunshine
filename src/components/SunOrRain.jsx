3a3e05910aee443d8dfbbcf8dd6d52dc91090f28
Jethet <henriette.hettinga@protonmail.com>
2021-09-22 17:47:55 +0200

    Find alternatives for image path

--- src/components/SunOrRain.jsx
+++ src/components/SunOrRain.jsx
@@ -1,26 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const SunOrRain = () => {
-  const [sun, setSun] = useState(true)
+  const [image, setImage] = useState("sun-image");
 
-const handleClick = () => {
-  if (sun === true) {
-    setSun(false)
-    console.log("rain");
-  }
-  if (sun === false) {
-    setSun(true)
-    console.log("sun");
-  }
-}
+  const handleClick = () => {
+    if (image === "sun-image") {
+      setImage("rain-image");
+      console.log("rain");
+    }
+    if (image === "rain-image") {
+      setImage("sun-image");
+      console.log("sun");
+    }
+  };
 
-  return ( 
+  return (
     <div>
       <h1>Sunny or not?</h1>
-      <div className={sun === true ? "sun-image" : "rain-image"}></div>
+      <div className="background-image">
+        <img src={image} alt="weather" />
+      </div>
       <button onClick={handleClick}>Click me!</button>
     </div>
-   );
-}
- 
+  );
+};
+
 export default SunOrRain;
